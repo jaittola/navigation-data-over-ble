732e0bb43606d41dec852cc820c29e4e14574c6d
Jukka Aittola <jaittola@iki.fi>
2018-02-10 17:45:36 +0200

    Collect data keys and show then in drop-downs for devices;
     * so that user can select the value to be displayed

--- server/web-ui/device-data.js
+++ server/web-ui/device-data.js
@@ -4,9 +4,14 @@
         var navDataBody = document.getElementById("navigation-data-body")
         var devicesBody = document.getElementById("devices-body")
 
+        var dataKeys = new Set([])
+        var addDataKey = makeAddDataKeyFunction(dataKeys, devicesBody)
+
         var websocket = connectWebsocket()
         websocket.onmessage = function(event) {
-            handleEventData(event.data, navDataBody, devicesBody)
+            handleEventData(event.data, navDataBody, devicesBody, addDataKey,
+                            Array.from(dataKeys.values()).sort(),
+                           websocket)
         }
     }
 
@@ -15,15 +20,15 @@
         return new WebSocket(protocolPrefix + "//" + window.location.host + "/datastream")
     }
 
-    function handleEventData(dataAsString, navDataBody, devicesBody) {
+    function handleEventData(dataAsString, navDataBody, devicesBody, addDataKey, dataKeys, websocket) {
         try {
             var data = JSON.parse(dataAsString)
             switch (data.type) {
             case "navdata":
-                handleNavdata(data.value, navDataBody)
+                handleNavdata(data.value, navDataBody, addDataKey)
                 break
             case "devices":
-                handleDevices(data.value, devicesBody)
+                handleDevices(data.value, devicesBody, dataKeys, websocket)
                 break
             default:
             }
@@ -32,25 +37,27 @@
         }
     }
 
-    function handleNavdata(navdata, tableBody) {
+    function handleNavdata(navdata, tableBody, addDataKey) {
         if (navdata.path.match(/^(\w|\.)+$/)) {
-            setDataToTable("navdata-" + navdata.path, navdata.path, navdata.value, tableBody)
+            setDataToTable("navdata-" + navdata.path, navdata.path,
+                           navdata.value, tableBody, updateValueElement)
+            addDataKey(navdata.path)
         }
     }
 
-
-    function setDataToTable(rowId, title, value, tableBody) {
+    function setDataToTable(rowId, title, value, tableBody, setValueElementData) {
         var row = document.getElementById(rowId)
         if (row == null) {
-            insertValueToTable(tableBody, title, value, rowId)
+            insertValueToTable(tableBody, title, value, rowId, setValueElementData)
         } else {
             var valueElement = row.getElementsByClassName("navdata-value")[0]
             if (valueElement != null)
-                updateValueElement(valueElement, value)
+                setValueElementData(valueElement, value)
         }
     }
 
-    function insertValueToTable(tableBody, title, value, rowId) {
+    function insertValueToTable(tableBody, title, value, rowId,
+                                setValueElementData) {
         var row = tableBody.insertRow(-1)
         row.id = rowId
         var pathElement = row.insertCell(0)
@@ -58,7 +65,7 @@
         pathElement.textContent = title
         var valueElement = row.insertCell(1)
         valueElement.classList.add("navdata-value")
-        updateValueElement(valueElement, value)
+        setValueElementData(valueElement, value)
     }
 
     function updateValueElement(valueElement, value) {
@@ -72,12 +79,67 @@
         }
     }
 
-    function handleDevices(devicedata, tableBody) {
-        console.log("Got devices: " + JSON.stringify(devicedata))
+    function updateDeviceDataDropdown(dataKeys, device, websocket) {
+        return function(valueElement, value) {
+            valueElement.innerHTML = ""
+            var select = document.createElement("select")
+            select.classList.add("data-type-selection")
+            setValuesInDropdown(select, dataKeys, value)
+            valueElement.appendChild(select)
+            select.onchange = function(event) {
+                sendSelectionToServer(websocket, device.id, event.target.value)
+            }
+        }
+    }
+
+    function handleDevices(devicedata, tableBody, dataKeys, websocket) {
         devicedata.forEach(function(device) {
             if (device.id.match(/^[0-9a-fA-F]+$/)) {
-                setDataToTable("devicedata-" + device.id, device.id, device.path, tableBody)
+                setDataToTable("devicedata-" + device.id, device.id,
+                               device.path, tableBody,
+                               updateDeviceDataDropdown(dataKeys, device, websocket))
             }
         })
+    }
+
+    function makeAddDataKeyFunction(dataKeys, deviceListTable) {
+        return function(dataKey) {
+            if (!dataKeys.has(dataKey)) {
+                dataKeys.add(dataKey)
+                updateDataValueDropdowns(dataKey, Array.from(dataKeys.values()).sort(), deviceListTable)
+            }
+        }
+    }
+
+    function updateDataValueDropdowns(newDataKey, allDataKeys, deviceListTable) {
+        Array.from(deviceListTable.getElementsByClassName("data-type-selection")).forEach(function(dropdown) {
+            var currentValue = dropdown.value
+            dropdown.innerHTML = ""
+            setValuesInDropdown(dropdown, allDataKeys, currentValue)
+        })
+    }
+
+    function setValuesInDropdown(dropdown, values, value) {
+        values.forEach(function(key) { addToDropdown(dropdown, key) })
+        var indexOfSelected = values.indexOf(value)
+        if (indexOfSelected >= 0) {
+            dropdown.selectedIndex = indexOfSelected
+        } else {
+            addToDropdown(dropdown, value)
+            dropdown.selectedIndex = values.length
+        }
+    }
+
+    function addToDropdown(dropdown, value) {
+        var option = document.createElement("option")
+        option.text = value
+        option.value = value
+        dropdown.add(option)
+    }
+
+    function sendSelectionToServer(websocket, deviceId, selection) {
+        websocket.send(JSON.stringify({ type: "selectionOfOutput",
+                                        id: deviceId,
+                                        selection: selection }))
     }
 })();
